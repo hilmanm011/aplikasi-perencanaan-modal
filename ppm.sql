6bf1adc21534fad2339b1dca4c11b78189a2d859
Muhamad Hilman <muhammadh@thebigrichgroup.com>
2020-07-25 15:12:50 +0700

    mengubah database

--- ppm.sql
+++ ppm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2020 at 10:25 PM
+-- Generation Time: Jul 25, 2020 at 10:11 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -64,58 +64,6 @@ INSERT INTO `dashboard` (`id`, `nama`, `nrp`, `email`, `jurusan`) VALUES
 (30, 'Sandhika Galih', '043040023', 'sandhikagalih@unpas.ac.id', 'Teknik Informatika'),
 (31, 'Nofariza Handayani', '083030030', 'nofa@yahoo.com', 'Teknik Lingkungan'),
 (33, 'Erik Doank', '133040123', 'erik@gmail.com', 'Teknik Mesin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `datapenduduk`
---
-
-CREATE TABLE `datapenduduk` (
-  `id` int(11) NOT NULL,
-  `provinsi` varchar(50) NOT NULL,
-  `jumlah` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `datapenduduk`
---
-
-INSERT INTO `datapenduduk` (`id`, `provinsi`, `jumlah`) VALUES
-(1, 'Aceh', 5189500),
-(2, 'Bali', 4246500),
-(3, 'Banten', 12448200),
-(4, 'Bengkulu', 1934300),
-(5, 'DI Yogyakarta', 3762200),
-(6, 'DKI Jakarta', 10374200),
-(7, 'Gorontalo', 1168200),
-(8, 'Jambi', 3515000),
-(9, 'Jawa Barat', 48037600),
-(10, 'Jawa Tengah', 34257900),
-(11, 'Jawa Timur', 39293000),
-(12, 'Kalimantan Barat', 4932500),
-(13, 'Kalimantan Selatan', 4119800),
-(14, 'Kalimantan Tengah', 2605300),
-(15, 'Kalimantan Timur', 3575400),
-(16, 'Kalimantan Utara', 691100),
-(17, 'Kepulauan Bangka Belitung', 1430900),
-(18, 'Kepulauan Riau', 2082700),
-(19, 'Lampung', 8289600),
-(20, 'Maluku', 1744700),
-(21, 'Maluku Utara', 1209300),
-(22, 'Nusa Tenggara Barat', 4955600),
-(23, 'Nusa Tenggara Timur', 5287300),
-(24, 'Papua', 3265200),
-(25, 'Papua Barat', 915400),
-(26, 'Riau', 6657900),
-(27, 'Sulawesi Barat', 1331000),
-(28, 'Sulawesi Selatan', 8690300),
-(29, 'Sulawesi Tengah', 2966300),
-(30, 'Sulawesi Tenggara', 2602400),
-(31, 'Sulawesi Utara', 2461000),
-(32, 'Sumatera Barat', 5321500),
-(33, 'Sumatera Selatan', 8267000),
-(34, 'Sumatera Utara', 14262100);
 
 -- --------------------------------------------------------
 
@@ -237,7 +185,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `username`, `password`, `image`, `role_id`, `is_active`) VALUES
-(5, 'Hasanul Ramadhan', 'hasanul', '$2y$10$tUv/q46oNEnmVyZ3mWccG.N3o8CUzAvch5vW4998G5Hlza0MjRx.2', 'default.jpg', 2, 1);
+(5, 'Hasanul Ramadhan', 'hasanul', '$2y$10$tUv/q46oNEnmVyZ3mWccG.N3o8CUzAvch5vW4998G5Hlza0MjRx.2', 'default.jpg', 2, 1),
+(6, 'Muhamad Hilman', 'hilman', '$2y$10$orQn4759Cv2EjXHKxi3Vee2Im9E1s2Z8SOaxVn5KmzQHYPEqeOFSu', 'default.jpg', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -272,12 +221,6 @@ ALTER TABLE `akun`
 -- Indexes for table `dashboard`
 --
 ALTER TABLE `dashboard`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `datapenduduk`
---
-ALTER TABLE `datapenduduk`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -333,12 +276,6 @@ ALTER TABLE `dashboard`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
--- AUTO_INCREMENT for table `datapenduduk`
---
-ALTER TABLE `datapenduduk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
-
---
 -- AUTO_INCREMENT for table `estimasi`
 --
 ALTER TABLE `estimasi`
@@ -366,7 +303,7 @@ ALTER TABLE `pembayaran`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_role`
